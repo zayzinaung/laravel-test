a8e9f722f6fde2cb95863e3c4335a2051f208442
Zay <zayzinaung1993@gmail.com>
2018-05-26 10:05:57 +0800

    First commit for Laravel Passport API

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Zay Zin Aung',
+            'email' => 'zayzinaung1993@gmail.com',
+            'password' => bcrypt('123456'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
